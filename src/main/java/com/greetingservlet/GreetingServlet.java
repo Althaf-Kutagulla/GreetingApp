f8ab696ffa74f84266203dc2982fcbb4afd413ca
Althaf <kutagullaalthaf12@gmail.com>
2023-09-26 19:28:03 +0530

    First Commit

---
+++ src/main/java/com/greetingservlet/GreetingServlet.java
@@ -0,0 +1,38 @@
+package com.greetingservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class GreetingServlet extends HttpServlet {
+
+	public void  doGet(HttpServletRequest req, HttpServletResponse res) {
+		
+		String name = (String) req.getParameter("personName");
+		
+		try {
+			PrintWriter out = res.getWriter();
+			System.out.println(name.length());
+			if(name.length() != 0 ) {
+				out.println("Hello "+name+", have a nice day!");
+			}else {
+				out.println("Enter correct input");
+				
+			}
+			
+			
+		}catch( IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+	
+	
+}
